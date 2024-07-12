46872f97d29e3add6f9ff0cb1ecc2ef176df3d79
drewkeat <drewjkeat@gmail.com>
2024-07-12 16:51:24 -0600

    install supabase packages and add mui login template

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,12 +1,12 @@
 import { Container, Paper, Typography } from "@mui/material";
+import Signinform from "@/components/Signinform"
+
 
 export default function Login() {
   return (
     <Container>
       <Typography variant="h3" color="green" align="center">Login Page</Typography>
-      <Paper elevation={5}>
-        <Typography variant="h4" color="blue" align="center">Login Form</Typography>
-      </Paper>
+        <Signinform />
     </Container>
   )
 }

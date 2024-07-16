72a83bdca445f06c0f33b301868602869ae8f187
drewkeat <drewjkeat@gmail.com>
2024-07-16 13:20:14 -0600

    generate dev and prod env files, test middleware and local supabase db

--- src/app/whiskeys/page.tsx
+++ src/app/whiskeys/page.tsx
@@ -1,7 +1,30 @@
 import { Typography } from "@mui/material";
+import { createClient } from "@/utils/supabase/server";
+
+const supabase = createClient()
+
+async function getWhiskeys(){
+  let { data: whiskeys, error } = await supabase
+    .from('whiskeys')
+    .select()
+    if(whiskeys){
+      return whiskeys
+    }
+    console.error(error)
+}
 
 export default function WhiskeyList() {
+ async function listWhiskeys(){
+  const whiskeys = await getWhiskeys()
+  return whiskeys?.map(whiskey => <li key={whiskey.id}>{whiskey.name}</li>)
+ }
+
   return (
-    <Typography variant="h3" color="green" align="center">Whiskey List</Typography>
+    <div>
+      <Typography variant="h3" color="green" align="center">Whiskey List</Typography>
+      <ul>
+        {listWhiskeys()}
+      </ul>
+    </div>
   )
 }
--- src/middleware.ts
+++ src/middleware.ts
@@ -2,7 +2,6 @@ import { type NextRequest } from 'next/server'
 import { updateSession } from '@/utils/supabase/middleware'
 
 export async function middleware(request: NextRequest) {
-  console.log('middleware hit')
   // return await updateSession(request)
 }
 

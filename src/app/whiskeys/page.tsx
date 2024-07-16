ace6930e2112bebda08cc3265f0a47c6de087262
drewkeat <drewjkeat@gmail.com>
2024-07-16 15:18:18 -0600

    add whiskeys to local env

--- src/app/whiskeys/page.tsx
+++ src/app/whiskeys/page.tsx
@@ -1,11 +1,21 @@
-import { Typography } from "@mui/material";
+import { Typography, Container } from "@mui/material";
+import { PrismaClient } from "@prisma/client";
 
+const prisma = new PrismaClient()
 
 export default async function WhiskeyList() {
-
+  async function listWhiskeys(){
+    const whiskeys = await prisma.whiskey.findMany()
+    return whiskeys.map(w => <li key={w.id}><a href={w.whiskyLink}>{w.name}</a></li>)
+  }
   return (
     <div>
       <Typography variant="h3" color="green" align="center">Whiskey List</Typography>
+      <Container maxWidth="md">
+        <ul>
+          {listWhiskeys()}
+        </ul>
+      </Container>
     </div>
   )
 }

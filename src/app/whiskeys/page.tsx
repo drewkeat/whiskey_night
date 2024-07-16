809dd92f38ffaa7fc0590487ab9e7d67d6abdaae
drewkeat <drewjkeat@gmail.com>
2024-07-16 14:20:43 -0600

    reset db with prisma configured

--- src/app/whiskeys/page.tsx
+++ src/app/whiskeys/page.tsx
@@ -1,25 +1,11 @@
 import { Typography } from "@mui/material";
-import { createClient } from "@/utils/supabase/server";
-import { PrismaClient } from '@prisma/client'
 
-const prisma = new PrismaClient()
-
-const getUsers = async() => {
-  // Run inside `async` function
-  const allUsers = await prisma.user.findMany()
-  console.log(allUsers)
-  return allUsers
-}
 
 export default async function WhiskeyList() {
-  async function listUsers(){
-    const users = await getUsers()
-    return users.map(u => <li key={u.id}>{u.email} - {u.name}</li>)
-  }
+
   return (
     <div>
       <Typography variant="h3" color="green" align="center">Whiskey List</Typography>
-      <ul>{listUsers()}</ul>
     </div>
   )
 }

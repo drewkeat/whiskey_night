e25cc9321158f4248d45dccb206902bc658936f8
drewkeat <drewjkeat@gmail.com>
2024-08-04 13:59:02 -0600

    make scrape-search return mini-entries from scraping. Also, store thumbnails...

--- supabase/functions/scrape-search/index.ts
+++ supabase/functions/scrape-search/index.ts
@@ -1,43 +1,82 @@
 // Follow this setup guide to integrate the Deno language server with your editor:
 // https://deno.land/manual/getting_started/setup_your_environment
 // This enables autocomplete, go to definition, etc.
-import axios from "npm:axios"
-import * as cheerio from "npm:cheerio"
-import { corsHeaders } from "../cors.ts"
-
-console.log("Running Test Function")
+import axios from "npm:axios";
+import * as cheerio from "npm:cheerio";
+import { corsHeaders } from "../cors.ts";
+import { createClient } from "jsr:@supabase/supabase-js@2";
 
 Deno.serve(async (req) => {
-  if (req.method === 'OPTIONS') {
-    return new Response('ok', { headers: corsHeaders })
-  }
+  const authHeader = req.headers.get("Authorization")!;
+  const supabaseClient = createClient(
+    Deno.env.get("SUPABASE_URL") ?? "",
+    Deno.env.get("SUPABASE_ANON_KEY") ?? "",
+    { global: { headers: { Authorization: authHeader } } },
+  );
 
-  let reqData
-  try {
-    reqData = await req.json()
-  } catch (error) {
-    console.error("Request JSON not parsable", error)
-  }
+  const searchTerm: string = await req.json().then((data) => data.term);
+  const html: string = await axios.get(
+    `https://distiller.com/search?term=${searchTerm}`,
+  ).then((r) => r.data);
 
-  if(reqData && reqData.term){
-    const html = await axios.get(`https://distiller.com/search?term=${reqData.term}`).then(r => r.data)
-    const $ = cheerio.load(html)
-    const whiskeys = $('.spirit.whiskey-content').map((_i,e)=> ({
-      name: $(e).find('.name').text().trim(),
-      link: `https://www.distiller.com`+$(e).find('a').attr('href'),
-      imageURL: $(e).find('.image').attr('style')!.match(/background.+url\((.+)\)/)![1]
-    })).get()
-    
+  const uploadImage = async (
+    name: string,
+    imgUrl: string,
+  ): Promise<{ path: string | null }> => {
+    const response = await fetch(imgUrl);
+    const imgData = await response.arrayBuffer();
+    const imgBlob = new Blob([imgData], { type: "image/jpeg" });
+    const imgFile = new File([imgBlob], `${name}-${Date.now()}.jpg`, {
+      type: "image/jpeg",
+    });
 
-    
+    if (imgData) {
+      const { data, error } = await supabaseClient.storage.from("imgs").upload(
+        "whiskey-pics/thumbnails/" + name,
+        imgFile,
+      );
+      if (error) {
+        console.error(error);
+        return { path: null };
+      }
+      return data;
+    }
+    return { path: null };
+  };
+
+  if (html) {
+    const $ = cheerio.load(html);
+    const results = $(".results-container li.spirit");
+    const entries = await Promise.all([...results].map(async (item) => {
+
+      const itemName = $(item).find(".name-content").text().trim()
+      const imgLink = $(item).find(".image").attr("style")?.match(
+        /background-image: *url *\((.+?)\)/,
+      )?.[1] || ""
+
+      const imgData = await uploadImage(itemName+"-thumbnail", imgLink)
+
+      return (
+        {
+          name: itemName,
+          link: $(item).find("a").attr("href"),
+          imgLink: imgData.path,
+        }
+      );
+    }));
+
+    console.log(entries);
     return new Response(
-      JSON.stringify(whiskeys), {headers: {...corsHeaders}}
-    )
+      JSON.stringify(entries),
+      { headers: { ...corsHeaders } },
+    );
   }
   return new Response(
-    "Could not find whiskey", {headers: {...corsHeaders}}
-  )
-})
+    JSON.stringify({
+      message: "There was an issue with scraping a whiskey for this term",
+    }),
+  );
+});
 
 /* To invoke locally:
 

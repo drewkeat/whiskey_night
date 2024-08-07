afdcbffdb7f632f893f44eb3fe11113cba1ef173
drewkeat <drewjkeat@gmail.com>
2024-08-06 22:44:22 -0600

    implement duplicateCheck by whiskeyLink in scrape-whiskey, add scrape-seed-by-name, update scrape-seed to use functions.invoke('scrape-whiskey')

--- supabase/functions/scrape-whiskey/index.ts
+++ supabase/functions/scrape-whiskey/index.ts
@@ -1,8 +1,3 @@
-// Follow this setup guide to integrate the Deno language server with your editor:
-// https://deno.land/manual/getting_started/setup_your_environment
-// This enables autocomplete, go to definition, etc.
-
-// Setup type definitions for built-in Supabase Runtime APIs
 import "jsr:@supabase/functions-js/edge-runtime.d.ts"
 import axios from "npm:axios"
 import * as cheerio from "npm:cheerio"
@@ -10,12 +5,22 @@ import { createClient } from 'jsr:@supabase/supabase-js@2'
 
 Deno.serve(async (req) => {
   const authHeader = req.headers.get('Authorization')!
-  const supabaseClient = createClient(
+  const supabase = createClient(
     Deno.env.get('SUPABASE_URL') ?? '',
     Deno.env.get('SUPABASE_ANON_KEY') ?? '',
     { global: { headers: { Authorization: authHeader } } }
   )
 
+  const duplicateCheck = async (urlToCheck:string) => {
+    const {data, error} = await supabase.from('whiskey').select('whiskeyLink')
+    if(error){
+      console.error(error)
+      return
+    }
+    const existingLinks =  data.map((item: {whiskeyLink: string}) => item.whiskeyLink)
+    return existingLinks.includes(urlToCheck) ? true : false
+  }
+  
   const uploadImage = async (name: string, imgUrl: string): Promise<{path: string|null}> => {
     const response = await fetch(imgUrl)
     const imgData = await response.arrayBuffer()
@@ -23,7 +28,7 @@ Deno.serve(async (req) => {
     const imgFile = new File([imgBlob], `${name}-${Date.now()}.jpg`, { type: 'image/jpeg' });
 
     if (imgData){
-      const {data, error} = await supabaseClient.storage.from('imgs').upload("whiskey-pics/"+name, imgFile)
+      const {data, error} = await supabase.storage.from('imgs').upload("whiskey-pics/"+name, imgFile)
       if(error){
         console.error(error)
         return {path: null}
@@ -35,6 +40,10 @@ Deno.serve(async (req) => {
 
   const { url } = await req.json()
   console.log(url)
+  if(await duplicateCheck(url)){
+    console.log("Duplicate record exists")
+    return new Response(JSON.stringify("Duplicate Bottle Exists"))
+  }
   const html = (await axios.get(url)).data
   const $ = cheerio.load(html)
 
@@ -51,14 +60,14 @@ Deno.serve(async (req) => {
   const caskType = $('.detail.cask-type .value').text()
   const flavorProfile = $('.js-flavor-profile-chart').data('flavors')
 
-  const imgData = await uploadImage(name, whiskeyImg)
+  const imgData = whiskeyImg ? await uploadImage(name, whiskeyImg) : {path: ''}
   console.log(imgData)
   
   const obj = {
     name, type, distillery, location, description, whiskeyImg: imgData.path, whiskeyLink, age, abv, style, caskType, flavorProfile
   }
 
-  const {data, error} = await supabaseClient.from('whiskey').insert(obj).select()
+  const {data, error} = await supabase.from('whiskey').insert(obj).select()
 
   if(data){
       return new Response(

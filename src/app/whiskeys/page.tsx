3913a15b5a73dee8e4008c58cecd2ba91f9b0c9c
drewkeat <drewjkeat@gmail.com>
2024-09-08 16:34:51 -0600

    remove whiskeys pages to start fresh

--- src/app/whiskeys/page.tsx
+++ src/app/whiskeys/page.tsx
@@ -1,27 +1,15 @@
 import { Typography, Container } from "@mui/material";
 import { createClient } from "@/utils/supabase/server";
-import { Database } from "@/types/supabase_types";
+
 
 export default async function WhiskeyList() {
-  
-  const supabase = createClient();
-
-  async function listWhiskeys() {
-    const { error, data } = await supabase.from("whiskey").select();
-    error && console.error("error", error)
-    return data?.map((w) => (
-      <li key={w.id}>
-        <a href={w.whiskeyLink || undefined}>{w.name}</a>
-      </li>
-    ));
-  }
   return (
     <div>
       <Typography variant="h3" color="green" align="center">
         Whiskey List
       </Typography>
       <Container maxWidth="md">
-        <ul>{listWhiskeys()}</ul>
+        <ul>List Whiskeys here</ul>
       </Container>
     </div>
   );

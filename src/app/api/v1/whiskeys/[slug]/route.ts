e164200ace3840ea97695ce5cac568962669103f
drewkeat <drewjkeat@gmail.com>
2024-10-02 15:20:48 -0600

    generate initial api routes

---
+++ src/app/api/v1/whiskeys/[slug]/route.ts
@@ -0,0 +1,12 @@
+import { NextRequest, NextResponse } from "next/server";
+import { createClient } from "@/utils/supabase/server";
+
+export async function GET(request: NextRequest, {params}: {params: {slug: string}}){
+  const supabase = createClient()
+  const whiskeyId = params.slug
+  const {data: whiskey, error} = await supabase.from('whiskey').select().eq("id", whiskeyId)
+  if(error){
+    return NextResponse.json(error)
+  }
+  return NextResponse.json(whiskey)
+}
---
+++ src/app/api/v1/whiskeys/route.ts
@@ -0,0 +1,11 @@
+import { NextRequest, NextResponse } from "next/server";
+import { createClient } from '@/utils/supabase/server'
+
+export async function GET(request: NextRequest){
+  const supabase = createClient()
+  const {data: whiskeys, error} = await supabase.from('whiskey').select()
+  if (error) {
+    return NextResponse.json(error)
+  } 
+  return NextResponse.json(whiskeys)
+}

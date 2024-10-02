077d415e55a4fdb3e8d82d901a09400e751b34cd
drewkeat <drewjkeat@gmail.com>
2024-10-01 19:40:16 -0600

    initial work for whiskeys list page

--- src/app/whiskeys/page.tsx
+++ src/app/whiskeys/page.tsx
@@ -1,14 +1,50 @@
-import { Typography, Container } from "@mui/material";
-
+import { Typography, Container, Box, Card, List, ListItem } from "@mui/material";
+import Image from "next/image";
+import { createClient } from "@/utils/supabase/server";
 
 export default async function WhiskeyList() {
+  const supabase = createClient();
+  const { data: whiskeys, error } = await supabase.from("whiskey").select();
+  if (error) {
+    return error;
+  }
+
+  const listWhiskeys = async () => {
+    return whiskeys.map((whiskey) => {
+      let img;
+      if (whiskey.whiskeyImg) {
+        const { data } = supabase.storage
+          .from("imgs")
+          .getPublicUrl(whiskey.whiskeyImg);
+        img = data.publicUrl;
+      }
+      return (
+        <Card key={`whiskey-${whiskey.id}`} elevation={4}>
+          <ListItem divider>
+            {img && (
+              <Box sx={{flexGrow: 1}}>
+                <Image
+                  alt={whiskey.name + "-image"}
+                  src={img as string}
+                  width={50}
+                  height={50}
+                />
+              </Box>
+            )}
+            <Typography>{whiskey.name}</Typography>
+          </ListItem>
+        </Card>
+      );
+    });
+  };
+
   return (
     <div>
       <Typography variant="h3" color="green" align="center">
         Whiskey List
       </Typography>
-      <Container maxWidth="md">
-        <ul>List Whiskeys here</ul>
+      <Container disableGutters>
+        <List>{listWhiskeys()}</List>
       </Container>
     </div>
   );

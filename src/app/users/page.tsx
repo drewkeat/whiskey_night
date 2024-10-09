920d05bef6eb8ed030669289347fb976b0793ba1
Drew Keat <drewjkeat@gmail.com>
2024-10-08 21:47:45 -0600

    test pr

--- src/app/users/page.tsx
+++ src/app/users/page.tsx
@@ -2,6 +2,9 @@ import { Typography } from "@mui/material";
 
 export default function User() {
   return (
-    <Typography variant="h3" color="green" align="center">User List</Typography>
+	<>
+		<Typography variant="h3" color="green" align="center">User List</Typography>
+		<Typography variant="h3" color="green" align="center">Test PR</Typography>
+	</>
   )
 }

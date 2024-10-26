b3254bbf4db4b8d8a52f4116458e4e4c68315cf6
drewkeat <drewjkeat@gmail.com>
2024-10-26 17:57:51 -0600

    update whiskey[slug] to use whiskeyDetailsCard

--- src/app/whiskeys/[slug]/page.tsx
+++ src/app/whiskeys/[slug]/page.tsx
@@ -1,20 +1,11 @@
 import React from "react";
-import Image from "next/image";
 import {
-  Box,
-  Container,
-  Card,
-  Divider,
-  CardContent,
-  Typography,
-  Slider,
-  Stack
-} from "@mui/material";
+  Container} from "@mui/material";
 
 import {
   getWhiskey,
-  getWhiskeyImg,
 } from "@/utils/supabase/queries/serverQueries";
+import WhiskeyDetailsCard from "./components/WhiskeyDetailsCard";
 
 type Props = {
   params: { slug: string };
@@ -22,158 +13,11 @@ type Props = {
 
 async function WhiskeyDetailsPage({ params }: Props) {
   const whiskey = await getWhiskey(decodeURI(params.slug));
-  const whiskeyImage = getWhiskeyImg(whiskey);
 
-  const buildProfile = () => {
-    if (whiskey.flavorProfile) {
-      const options = Object.entries(whiskey.flavorProfile);
-      return options.map((o, i) => {
-        return (
-          <Box height={100} key={whiskey.id + "-" + o[0]+i}>
-            <Slider
-              defaultValue={o[1]}
-              orientation="vertical"
-              step={5}
-              marks
-              min={0}
-              max={100}
-              name="read-only"
-              disabled
-              sx={{color: 'secondary.main', '& .Mui-disabled': {color: 'primary.main'}, width: 2}}
-              />
-              <Typography sx={{transform: {xs: "rotate(90deg) translate(.5rem, -1rem)", md: "none"}}}>{o[0]}</Typography>
-          </Box>
-        );
-      });
-    }
-  };
 
   return (
     <Container sx={{ mt: 5 }}>
-      <Card elevation={8} sx={{paddingBottom: 5}}>
-        <Typography
-          variant="h4"
-          bgcolor={"secondary.main"}
-          color={"secondary.contrastText"}
-          padding={1}
-          textAlign={"center"}
-          sx={{ ":hover": { backgroundColor: "secondary.light" } }}
-        >
-          {whiskey.name}
-        </Typography>
-        <Divider variant="fullWidth" />
-        <CardContent>
-          <Box sx={{ display: "flex" }}>
-            <Box flexGrow={1}>
-              <Typography variant="subtitle1">
-                Distillery:
-                <Typography
-                  variant="subtitle1"
-                  component={"span"}
-                  fontStyle={"italic"}
-                >
-                  {" " + whiskey.distillery}
-                </Typography>
-              </Typography>
-              <Typography variant="subtitle1" width={"100%"}>
-                Location:{" "}
-                <Typography
-                  variant="subtitle1"
-                  component={"span"}
-                  fontStyle={"italic"}
-                >
-                  {" " + whiskey.location}
-                </Typography>
-              </Typography>
-            </Box>
-            <Box>
-              <Typography variant="subtitle1">
-                Type:{" "}
-                <Typography
-                  variant="subtitle1"
-                  component={"span"}
-                  fontStyle={"italic"}
-                >
-                  {" " + whiskey.type}
-                </Typography>
-              </Typography>
-              <Typography variant="subtitle1" flexGrow={1}>
-                ABV:
-                <Typography
-                  variant="subtitle1"
-                  component={"span"}
-                  fontStyle={"italic"}
-                >
-                  {" " + whiskey.abv}
-                </Typography>
-              </Typography>
-            </Box>
-          </Box>
-          {whiskeyImage && (
-            <Box
-              display={"flex"}
-              justifyContent={"center"}
-              position={"relative"}
-            >
-              <Image
-                src={whiskeyImage}
-                height={150}
-                width={150}
-                alt={whiskey.name + "-image"}
-              />
-            </Box>
-          )}
-          <Box sx={{ display: "flex", flexWrap: "wrap", padding: 0 }}>
-            <Typography variant="subtitle1" flexGrow={1}>
-              Cask Type:
-              <Typography
-                variant="subtitle1"
-                component={"span"}
-                fontStyle={"italic"}
-              >
-                {" " + whiskey.caskType}
-              </Typography>
-            </Typography>
-            <Typography variant="subtitle1" textAlign={"end"}>
-              Age:
-              <Typography
-                variant="subtitle1"
-                component={"span"}
-                fontStyle={"italic"}
-              >
-                {" " + whiskey.age}
-              </Typography>
-            </Typography>
-          </Box>
-        </CardContent>
-        <Divider variant="fullWidth" />
-        <CardContent>
-          <Typography variant="body2" color="textSecondary" component="p">
-            {whiskey.description}
-          </Typography>
-        </CardContent>
-        <CardContent
-          sx={{
-            display: {md: "flex"} ,
-            justifyContent: "center"
-          }}
-        >
-          <Stack
-            direction={"row"}
-            spacing={1}
-            minHeight={"fit-content"}
-            paddingBottom={1}
-            sx={{
-              "& > *": {
-                flexShrink: 0,
-                width: { xs: "4%", md: "auto" }
-              }
-            }}
-          >
-            {buildProfile()}
-          </Stack>
-        </CardContent>
-      </Card>
+      <WhiskeyDetailsCard whiskey={whiskey}/>
     </Container>
   );
 }
--- supabase/migrations/20240803201300_create_profiles_table.sql
+++ supabase/migrations/20240803201300_create_profiles_table.sql
@@ -1,30 +1,30 @@
--- create profiles table
-create table public.profiles (
-  id uuid not null references auth.users on delete cascade,
-  firstName text,
-  lastName text,
-  email text,
+-- -- create profiles table
+-- create table public.profiles (
+--   id uuid not null references auth.users on delete cascade,
+--   firstName text,
+--   lastName text,
+--   email text,
 
-  primary key (id)
-);
+--   primary key (id)
+-- );
 
-alter table public.profiles enable row level security;
+-- alter table public.profiles enable row level security;
 
--- inserts a row into public.profiles
-create function public.handle_new_user()
-returns trigger
-language plpgsql
-security definer set search_path = ''
-as $$
-begin
-  insert into public.profiles (id, firstName, lastName, email)
-  values (new.id, new.raw_user_meta_data ->> 'firstName', new.raw_user_meta_data ->> 'lastName', new.email);
-  return new;
-end;
-$$;
+-- -- inserts a row into public.profiles
+-- create function public.handle_new_user()
+-- returns trigger
+-- language plpgsql
+-- security definer set search_path = ''
+-- as $$
+-- begin
+--   insert into public.profiles (id, firstName, lastName, email)
+--   values (new.id, new.raw_user_meta_data ->> 'firstName', new.raw_user_meta_data ->> 'lastName', new.email);
+--   return new;
+-- end;
+-- $$;
 
--- trigger the function every time a user is created
-create trigger on_auth_user_created
-  after insert on auth.users
-  for each row execute procedure public.handle_new_user();
+-- -- trigger the function every time a user is created
+-- create trigger on_auth_user_created
+--   after insert on auth.users
+--   for each row execute procedure public.handle_new_user();
 
